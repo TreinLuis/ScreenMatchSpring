96d8ec16208f53d9619bcc8c5f52d332b211a41d
TreinLuis <21luiseduardo2005@gmail.com>
2024-05-20 14:20:06 -0300

    FirstCommit

---
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -0,0 +1,12 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//JsonAlias serve para atribuir um apelido ao campo da API que determinamos dentro dos parenteses
+
+@JsonIgnoreProperties(ignoreUnknown = true)//Este comando ignora os dados que nao foram listados por mim na record
+public record DadosSerie(@JsonAlias("Title")String titulo,
+                         @JsonAlias("imdbRating") String avaliação,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas) {
+
+}
